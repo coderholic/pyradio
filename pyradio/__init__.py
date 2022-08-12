30f85817d5a072b2e1537ad0f5b65f082726bc8f
Spiros Georgaras <sng@hellug.gr>
2022-08-12 09:45:58 +0300

    version 0.8.9.24 (0.9-beta21)

--- pyradio/__init__.py
+++ pyradio/__init__.py
@@ -1,6 +1,6 @@
 " pyradio -- Console radio player. "
 
-version_info = (0, 8, 9, 23)
+version_info = (0, 8, 9, 24)
 
 # Set it to True if new stations have been
 # added to the package's stations.csv
