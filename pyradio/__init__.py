946a476e843be737688e87ae8b1438986207b6fb
Spiros Georgaras <sng@hellug.gr>
2023-01-10 19:27:30 +0200

    version 0.8.9.35 (0.9-beta32) fixing call to fix_pyradio_desktop_file during installation

--- pyradio/__init__.py
+++ pyradio/__init__.py
@@ -1,6 +1,6 @@
 " pyradio -- Console radio player. "
 
-version_info = (0, 8, 9, 34)
+version_info = (0, 8, 9, 35)
 
 # Set it to True if new stations have been
 # added to the package's stations.csv
