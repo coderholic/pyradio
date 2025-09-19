b2c3580518d8f1854c61cde6176ad9595cca1f77
Spiros Georgaras <sng@hellug.gr>
2025-09-19 18:14:18 +0300

    version 0.9.3.11.19 - 0.9.3.12-beta19

--- pyradio/install.py
+++ pyradio/install.py
@@ -17,7 +17,7 @@ from shutil import which
 ''' This is PyRadio version this
     install.py was released for
 '''
-PyRadioInstallPyReleaseVersion = '0.9.3.11.18'
+PyRadioInstallPyReleaseVersion = '0.9.3.11.19'
 
 locale.setlocale(locale.LC_ALL, "")
 
