075a1df7e42c6f971142b74e227bb26ad0ae1452
Spiros Georgaras <sng@hellug.gr>
2026-01-04 23:35:08 +0200

    version 0.9.3.11.22 - 0.9.3.12-beta22

--- pyradio/install.py
+++ pyradio/install.py
@@ -17,7 +17,7 @@ from shutil import which
 ''' This is PyRadio version this
     install.py was released for
 '''
-PyRadioInstallPyReleaseVersion = '0.9.3.11.21'
+PyRadioInstallPyReleaseVersion = '0.9.3.11.22'
 
 locale.setlocale(locale.LC_ALL, "")
 
